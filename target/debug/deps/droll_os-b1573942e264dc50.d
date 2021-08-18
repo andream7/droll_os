d860c7ecea30d73303f3f633643ef12e6c78562a
andream7 <2236407598@qq.com>
2021-08-18 11:51:08 +0800

    commit main

---
+++ target/debug/deps/droll_os-b1573942e264dc50.d
@@ -0,0 +1,5 @@
+/home/andream7/test/droll_os/target/debug/deps/droll_os-b1573942e264dc50.rmeta: src/main.rs
+
+/home/andream7/test/droll_os/target/debug/deps/droll_os-b1573942e264dc50.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/droll_os-ebcf2022a3624c27.d
@@ -0,0 +1,5 @@
+/home/andream7/test/droll_os/target/debug/deps/droll_os-ebcf2022a3624c27.rmeta: src/main.rs
+
+/home/andream7/test/droll_os/target/debug/deps/droll_os-ebcf2022a3624c27.d: src/main.rs
+
+src/main.rs:
